efa39d74c04dc9a9d30594eca8bba31504029a1c
lxqiang95 <lxq13925731395@163.com>
2020-12-10 14:28:24 +0800

    purchaseOrder

--- sql/WarehouseReceipt/warehousereceipt.py
+++ sql/WarehouseReceipt/warehousereceipt.py
@@ -7,3 +7,52 @@
 
 import pymysql
 
+
+def select_test():
+    """
+    id                               '主键'
+    purchase_sn                      '采购单号',
+    order_type                       '订单类型(0“样品订单”、1“赠品订单”，2“系统订单”，3“配件订单”，4“FBA订单”,5“额外加货订单”)',
+    bill_source                      '单据来源(0"系统创建",1"手动创建")',
+    purchase_status                  '采购单状态(0:初始状态，1:待厂商确认，2:拒单总裁审批，3:拒单完成，4:待付款，5:待发货，6:待收货，7:待入库，8:部分完成，9:待财务二审，10:二审完成，11:已完成)',
+    group_id                         '小组id',
+    buyer                            '采购员',
+    domestic_warehouse_id            '国内收货仓库id',
+    domestic_address                 '国内仓库地址',
+    overseas_warehouse_id            '海外收货仓库id',
+    estimated_domestic_arrival_date  '国内预计到货日期',
+    estimated_overseas_arrival_date  '海外预计到货日期',
+    estimated_loading_date           '预计装柜日期',
+    domestic_required_arrival_date   '国内要求到达日期',
+    domestic_required_delivery_date  '国内要求发货日期',
+    is_freeze                        '是否冻结(0否，1是)',
+    reconciliation_purchase_sn       '调仓采购确认单号',
+    procurement_supplier_id          '供应商id',
+    is_early_arrival_allowed         '是否允许早到货 0否 1是',
+    is_tax                           '是否含税（0否·,1是）',
+    is_visible                       '是否可见 1（拒单按钮） 2（驳回按钮）',
+    created_time                     '创建日期',
+    create_by                        '创建人',
+    update_time                      '更新时间',
+    update_by                        '更新人',
+    is_delete                        '是否删除 0未删除 1已删除'
+    """
+    # 创建连接对象
+    con = pymysql.connect(host='47.103.124.72', user='vevor', password='RNjIEP8q0e^pbQS$', database='scp', port=3306,
+                          charset='utf8')
+    # 获取游标cursor
+    cur = con.cursor()
+    # 执行数据库命令
+    sql = 'select * from procurement_purchase_order  where id =2'
+    cur.execute(sql)
+    # # 执行sql
+    # con.commit()
+    # 获取查询结果集
+    rs = cur.fetchall()
+    # 关闭连接
+    con.close()
+
+    print(rs)
+
+
+select_test()
--- testcase/purchaseOrder/purchaseOrder.py
+++ testcase/purchaseOrder/purchaseOrder.py
@@ -5,6 +5,9 @@
 # @File     : purchaseOrder.py
 # @Software : PyCharm
 from setting.purchaseOrder.sem_purchaseOrder import Sem_PurchaseOrder
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
 from time import sleep
 import unittest
 
@@ -13,12 +16,26 @@ class PurchaseOrder(unittest.TestCase):
 
     def setUp(self) -> None:
         spo = Sem_PurchaseOrder()
-        global driver
-        driver = spo.PurchaseOrder()
+        self.driver = spo.PurchaseOrder()
+        self.wait = WebDriverWait(self.driver, 60)
 
     def test(self):
-        driver.minimize_window()
+        self.wait.until(ec.presence_of_element_located((By.XPATH, "//div[text()='供应链管理']")))
+        iframe_test = self.driver.find_element_by_xpath('//*[@id="iframs"]')
+        self.driver.switch_to.frame(iframe_test)
+        self.wait.until(ec.presence_of_element_located((By.ID, 'purchaseSn')))
+        # 输入采购单号
+        self.driver.find_element_by_id('purchaseSn').send_keys('CGD202007240001')
+        sleep(1)
+        # 点击查询
+        self.select_test()
+        # 操作
+        self.driver.find_element_by_xpath("//span[text()='操 作']").click()
         sleep(3)
 
+    def select_test(self):
+        # 点击查询
+        self.driver.find_element_by_xpath("//span[text()='查 询']").click()
+
     def tearDown(self) -> None:
-        driver.quit()
+        self.driver.quit()
