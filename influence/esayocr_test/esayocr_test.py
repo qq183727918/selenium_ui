54cf2567cf93f0c3cdc3aaf4d85875cff48947ee
lxqiang95 <183727918@qq.com>
2021-01-09 14:31:03 +0800

    [master]WarehouseReceipt

---
+++ influence/esayocr_test/esayocr_test.py
@@ -0,0 +1,14 @@
+# _*_ coding: UTF-8 _*_
+# @Time     : 2021/1/9 14:25
+# @Author   : LiuXiaoQiang
+# @Site     : http:www.cdtest.cn/
+# @File     : esayocr_test.py
+# @Software : PyCharm
+
+import easyocr
+
+reader = easyocr.Reader(['ch_sim', 'en'])
+
+result = reader.readtext('./123.png')
+
+print(result)
