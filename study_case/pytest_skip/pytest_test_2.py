acc624437970a38ae6a19e94abd41ae25c342802
lxqiang95 <183727918@qq.com>
2021-01-13 21:54:23 +0800

    [master]pytest-dependency

--- study_case/pytest_skip/pytest_test_2.py
+++ study_case/pytest_skip/pytest_test_2.py
@@ -12,17 +12,20 @@ class Test1:
 
     def explanation(self):
         """
-        先执行2，  测试用例1依赖测试用例2
+        测试用例1依赖测试用例2
+        只有test_2成功test_1才会执行，否则test_1跳过
         """
-
+    @pytest.mark.run(order=1)
     @pytest.mark.dependency(depends=['b'])
     def test_1(self):
-        assert True
+        print('test_2不通过，该条不执行')
+        # assert True
 
-    # @pytest.mark.run(order=0)
+    @pytest.mark.run(order=0)
     @pytest.mark.dependency(name="b")
     def test_2(self):
-        assert True
+        print('该条通过，test_1才能执行')
+        # assert True
 
 
 if __name__ == '__main__':
